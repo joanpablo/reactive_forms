19945acb1701208804485c4ad0be2e70f34f1169
Joan Pablo <johnpjm85@gmail.com>
2020-06-30 22:43:36 -0400

    - code documentation to FormGroup class

--- lib/models/form_group.dart
+++ lib/models/form_group.dart
@@ -2,10 +2,40 @@ import 'package:flutter/foundation.dart';
 import 'package:reactive_forms/models/form_control.dart';
 import 'package:reactive_forms/validators/form_group_validators.dart';
 
+/// Tracks the value and validity state of a group of FormControl instances.
+///
+/// A FormGroup aggregates the values of each child FormControl into one object,
+/// with each control name as the key.
+///
+/// It calculates its status by reducing the status values of its children.
+/// For example, if one of the controls in a group is invalid, the entire group
+/// becomes invalid.
+///
 class FormGroup extends ChangeNotifier {
   final Map<String, FormControl> _controls;
+
+  bool _valid = true;
+
+  /// These come in handy when you want to perform validation that considers
+  /// the value of more than one child control.
   final List<FormGroupValidatorFunction> validators;
 
+  /// Creates a new FormGroup instance.
+  ///
+  /// When instantiating a [FormGroup], pass in a collection of child controls
+  /// as the first argument.
+  ///
+  /// The key for each child registers the name for the control.
+  ///
+  /// ### Example:
+  ///
+  ///     final form = FromGroup({
+  ///       'name': FormControl(defaultValue: 'John Doe'),
+  ///       'email': FormControl(),
+  ///     });
+  ///
+  /// You can also set [validators] as optionally argument.
+  ///
   FormGroup(
     Map<String, FormControl> controls, {
     this.validators = const [],
@@ -19,15 +49,33 @@ class FormGroup extends ChangeNotifier {
     });
   }
 
-  bool _valid = true;
+  /// True if all the controls contained in this group are valid. If at least
+  /// one [FormControl] is invalid then the [FormGroup] is invalid.
   bool get valid => _valid;
 
+  /// True if at least one [FormControl] is invalid.
   bool get invalid => !this.valid;
 
+  /// Returns a [FormControl] by its name.
   FormControl formControl(String name) {
     return this._controls[name];
   }
 
+  /// Returns the current value of the group.
+  /// The values of controls as an object with
+  /// a key-value pair for each control in the group.
+  ///
+  /// ### Example:
+  ///
+  ///     final form = FormGroup({
+  ///       'name': FormControl(defaultValue: 'John Doe'),
+  ///       'email': FormControl(defaultValue: 'johndoe@email.com'),
+  ///     });
+  ///
+  ///     print(form.value);
+  ///
+  ///     { "name": "John Doe", "email": "johndoe@email.com" }
+  ///
   Map<String, dynamic> get value {
     final map = Map<String, dynamic>();
     this._controls.forEach((key, formControl) {
@@ -37,6 +85,11 @@ class FormGroup extends ChangeNotifier {
     return map;
   }
 
+  /// Resets all the form controls of the group, marking them as untouched,
+  /// and setting the [FormControl.value] to [FormControl.defaultValue].
+  ///
+  /// See also [FormControl.reset()]
+  ///
   void reset() {
     this._controls.forEach((key, formControl) {
       formControl.reset();
