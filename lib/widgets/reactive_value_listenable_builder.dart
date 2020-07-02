a14c7979f4445a58711c84e845de632eb553c675
Joan Pablo <johnpjm85@gmail.com>
2020-07-02 10:47:38 -0400

    - implemented ReactiveValueListenableBuilder

--- lib/widgets/reactive_value_listenable_builder.dart
+++ lib/widgets/reactive_value_listenable_builder.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:reactive_forms/exceptions/form_control_not_found_exception.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
-typedef ReactiveValueListenableBuilderCallback<T> = Widget Function(
-    BuildContext context, T value, Widget child);
-
-class ReactiveValueListenableBuilder<T> extends StatefulWidget {
+class ReactiveValueListenableBuilder<T> extends StatelessWidget {
   final String formControlName;
   final Widget child;
-  final ReactiveValueListenableBuilderCallback<T> builder;
+  final ValueWidgetBuilder<T> builder;
 
   const ReactiveValueListenableBuilder({
     Key key,
@@ -19,62 +15,12 @@ class ReactiveValueListenableBuilder<T> extends StatefulWidget {
         assert(builder != null);
 
   @override
-  _ReactiveValueListenableBuilderState<T> createState() =>
-      _ReactiveValueListenableBuilderState<T>();
-}
-
-class _ReactiveValueListenableBuilderState<T>
-    extends State<ReactiveValueListenableBuilder<T>> {
-  FormControl _control;
-
-  @override
-  void initState() {
-    final form = ReactiveForm.of(context, listen: false);
-    _control = form.formControl(widget.formControlName);
-    if (_control == null) {
-      throw FormControlNotFoundException(widget.formControlName);
-    }
-
-    this.subscribeFormControl();
-
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    final form = ReactiveForm.of(context, listen: false);
-    final newControl = form.formControl(widget.formControlName);
-    if (_control != newControl) {
-      this.unsubscribeFormControl();
-      _control = newControl;
-      subscribeFormControl();
-    }
-
-    super.didChangeDependencies();
-  }
-
-  @override
-  void dispose() {
-    this.unsubscribeFormControl();
-    super.dispose();
-  }
-
-  @protected
-  void subscribeFormControl() {
-    _control.onValueChanged.addListener(_onFormControlValueChanged);
-  }
-
-  @protected
-  void unsubscribeFormControl() {
-    _control.onValueChanged.removeListener(_onFormControlValueChanged);
-  }
-
-  void _onFormControlValueChanged() {
-    setState(() {});
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return widget.builder(context, _control.value, widget.child);
+    final form = ReactiveForm.of(context, listen: false);
+    final formControl = form.formControl(this.formControlName);
+    return ValueListenableBuilder<T>(
+      valueListenable: formControl.onValueChanged,
+      builder: this.builder,
+    );
   }
 }
