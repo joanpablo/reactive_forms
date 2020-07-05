0cc08fe6651b3957aff24bf5b919254e01e8edb9
Joan Pablo <johnpjm85@gmail.com>
2020-07-05 14:43:31 -0400

    - added code comment to Validation Message

--- lib/validators/validation_message.dart
+++ lib/validators/validation_message.dart
@@ -1,3 +1,17 @@
+/// This class is an utility for getting access to common names of
+/// validation messages.
+///
+/// ## Example
+///
+/// ```dart
+/// ReactiveTextField(
+///   formControlName: 'email',
+///   validationMessages: {
+///     ValidationMessage.required: 'The Email must not be empty',
+///     ValidationMessage.email: 'The Email must be a valid email'
+///   },
+/// );
+/// ```
 class ValidationMessage {
   static const String required = 'required';
   static const String pattern = 'pattern';
