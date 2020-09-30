3a7a121fce272a45d7613820dd237cb57ae35b5c
Joan Pablo <johnpjm85@gmail.com>
2020-09-30 09:02:17 -0400

    Release 6.0.0

--- lib/src/models/focus_controller.dart
+++ lib/src/models/focus_controller.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/foundation.dart';
 
+/// Represents a focus controller for a [FormControl].
 abstract class FocusController extends ChangeNotifier {
   bool _hasFocus = false;
 
+  /// Gets the focus state of the focus controller.
   bool get hasFocus => _hasFocus;
 
+  /// Update the focus state for controller.
+  ///
+  /// Provide the [hasFocus] argument to set the focus state.
+  ///
+  /// If [emitEvent] is true or not provided (the default) then an event is
+  /// triggered with the new focus state. If is false no event is triggered.
   void updateFocus(bool hasFocus, {bool emitEvent = true}) {
     if (_hasFocus != hasFocus) {
       _hasFocus = hasFocus;
@@ -14,5 +22,9 @@ abstract class FocusController extends ChangeNotifier {
     }
   }
 
+  /// This method is called from [FormControl] to notify the controller that
+  /// the focus in the control has changed.
+  ///
+  /// The [hasFocus] argument represents the state of the [FormControl].
   void onControlFocusChanged(bool hasFocus);
 }
