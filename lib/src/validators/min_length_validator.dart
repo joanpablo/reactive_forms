6a107d853d43f2b569687ddadbfc53f7e2d58b20
Joan Pablo <johnpjm85@gmail.com>
2020-10-13 18:42:19 -0400

    reimplemented Validators.minLength

--- lib/src/validators/min_length_validator.dart
+++ lib/src/validators/min_length_validator.dart
@@ -16,12 +16,24 @@ class MinLengthValidator extends Validator<dynamic> {
 
   @override
   Map<String, dynamic> validate(AbstractControl<dynamic> control) {
-    return (control.value == null || control.value.length >= this.minLength)
+    List<dynamic> collection;
+
+    if (control is FormArray<dynamic>) {
+      collection = control.value;
+    } else if (control is FormGroup) {
+      collection = control.value.keys.toList();
+    } else if (control is FormControl<Iterable<dynamic>>) {
+      collection = control.value.toList();
+    } else if (control is FormControl<String>) {
+      collection = control.value.runes.toList();
+    }
+
+    return (collection != null && collection.length >= this.minLength)
         ? null
         : {
             ValidationMessage.minLength: {
               'requiredLength': this.minLength,
-              'actualLength': control.value.length,
+              'actualLength': collection != null ? collection.length : 0,
             }
           };
   }
--- test/src/validators/validators_test.dart
+++ test/src/validators/validators_test.dart
@@ -38,16 +38,30 @@ void main() {
 
   group('MinLength Validator Tests', () {
     test('FormControl invalid if minLength invalid', () {
+      // Given: a invalid control
       final control = FormControl(
         value: 'Hello',
         validators: [Validators.minLength(6)],
       );
 
+      // Expect: control is invalid
       expect(control.invalid, true);
-      expect(control.errors[ValidationMessage.minLength] != null, true);
+      expect(control.errors.containsKey(ValidationMessage.minLength), true);
+    });
+
+    test('FormControl valid if minLength valid', () {
+      // Given: a valid control
+      final control = FormControl(
+        value: 'Reactive Forms',
+        validators: [Validators.minLength(6)],
+      );
+
+      // Expect: control is valid
+      expect(control.valid, true);
     });
 
     test('FormArray invalid if minLength invalid', () {
+      // Given: an invalid array
       final array = FormArray([
         FormControl(),
         FormControl(),
@@ -55,11 +69,13 @@ void main() {
         Validators.minLength(3)
       ]);
 
+      // Expect: array is invalid
       expect(array.invalid, true);
-      expect(array.errors[ValidationMessage.minLength] != null, true);
+      expect(array.errors.containsKey(ValidationMessage.minLength), true);
     });
 
     test('FormArray valid if minLength valid', () {
+      // Given: a valid array
       final array = FormArray([
         FormControl(),
         FormControl(),
@@ -68,7 +84,33 @@ void main() {
         Validators.minLength(3)
       ]);
 
+      // Expect: array is valid
       expect(array.valid, true);
+    });
+
+    test('FormGroup valid if minLength valid', () {
+      // Given: a valid group
+      final array = FormGroup({
+        'name': FormControl(),
+        'email': FormControl(),
+      }, validators: [
+        Validators.minLength(2)
+      ]);
+
+      // Expect: group is valid
+      expect(array.valid, true);
+    });
+
+    test('FormGroup invalid if minLength invalid', () {
+      // Given: an invalid group
+      final array = FormGroup({
+        'name': FormControl(),
+      }, validators: [
+        Validators.minLength(2)
+      ]);
+
+      // Expect: group is invalid
+      expect(array.valid, false);
     });
   });
 
