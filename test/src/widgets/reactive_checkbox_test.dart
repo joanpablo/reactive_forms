630d069c0a17176303c8b4c29ba681047361b66c
vasilich <vasilich@grafin.kiev.ua>
2021-12-22 19:03:14 +0200

    checkbox focus node

--- test/src/widgets/reactive_checkbox_test.dart
+++ test/src/widgets/reactive_checkbox_test.dart
@@ -11,7 +11,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with a boolean field in null
         final form = FormGroup({
-          'isChecked': FormControl<bool>(),
+          reactiveCheckboxTestingName: FormControl<bool>(),
         });
 
         // And: a checkbox is bind to boolean control
@@ -28,7 +28,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with a boolean field in null
         final form = FormGroup({
-          'isChecked': FormControl<bool>(),
+          reactiveCheckboxTestingName: FormControl<bool>(),
         });
 
         // And: a checkbox is bind to boolean control
@@ -48,7 +48,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with a boolean field in True
         final form = FormGroup({
-          'isChecked': FormControl<bool>(value: true),
+          reactiveCheckboxTestingName: FormControl<bool>(value: true),
         });
 
         // And: a checkbox is bind to boolean control
@@ -65,7 +65,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with a boolean field in False
         final form = FormGroup({
-          'isChecked': FormControl<bool>(value: false),
+          reactiveCheckboxTestingName: FormControl<bool>(value: false),
         });
 
         // And: a checkbox is bind to boolean control
@@ -82,7 +82,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with a boolean field
         final form = FormGroup({
-          'isChecked': FormControl<bool>(),
+          reactiveCheckboxTestingName: FormControl<bool>(),
         });
 
         // And: a checkbox is bind to boolean control
@@ -93,11 +93,11 @@ void main() {
         expect(checkbox.value, false);
 
         // When: set to True the form control
-        form.control('isChecked').value = true;
+        form.control(reactiveCheckboxTestingName).value = true;
         await tester.pump();
 
         // Then: the checkbox is checked
-        checkbox = tester.firstWidget(find.byType(Checkbox)) as Checkbox;
+        checkbox = tester.firstWidget<Checkbox>(find.byType(Checkbox));
         expect(checkbox.value, true);
       },
     );
@@ -107,7 +107,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with a boolean field in true
         final form = FormGroup({
-          'isChecked': FormControl<bool>(value: true),
+          reactiveCheckboxTestingName: FormControl<bool>(value: true),
         });
 
         // And: a checkbox is bind to boolean control
@@ -118,11 +118,11 @@ void main() {
         expect(checkbox.value, true);
 
         // When: set to False the form control
-        form.control('isChecked').value = false;
+        form.control(reactiveCheckboxTestingName).value = false;
         await tester.pump();
 
         // Then: the checkbox is checked
-        checkbox = tester.firstWidget(find.byType(Checkbox)) as Checkbox;
+        checkbox = tester.firstWidget<Checkbox>(find.byType(Checkbox));
         expect(checkbox.value, false);
       },
     );
@@ -132,14 +132,14 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form with disable control
         final form = FormGroup({
-          'isChecked': FormControl<bool>(disabled: true),
+          reactiveCheckboxTestingName: FormControl<bool>(disabled: true),
         });
 
         // When: a checkbox is bind to the form
         await tester.pumpWidget(ReactiveCheckboxTestingWidget(form: form));
 
         // Then: the checkbox is disabled
-        final checkbox = tester.firstWidget(find.byType(Checkbox)) as Checkbox;
+        final checkbox = tester.firstWidget<Checkbox>(find.byType(Checkbox));
         expect(checkbox.onChanged, null);
       },
     );
@@ -149,7 +149,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form
         final form = FormGroup({
-          'isChecked': FormControl<bool>(),
+          reactiveCheckboxTestingName: FormControl<bool>(),
         });
 
         // And: a checkbox is bind to the form
@@ -160,7 +160,7 @@ void main() {
         await tester.pump();
 
         // Then: the checkbox is disabled
-        final checkbox = tester.firstWidget(find.byType(Checkbox)) as Checkbox;
+        final checkbox = tester.firstWidget<Checkbox>(find.byType(Checkbox));
         expect(checkbox.onChanged, null);
       },
     );
@@ -170,7 +170,7 @@ void main() {
       (WidgetTester tester) async {
         // Given: a form
         final form = FormGroup({
-          'isChecked': FormControl<bool>(disabled: true),
+          reactiveCheckboxTestingName: FormControl<bool>(disabled: true),
         });
 
         // And: a checkbox is bind to the form
@@ -181,8 +181,154 @@ void main() {
         await tester.pump();
 
         // Then: the checkbox is enable
-        final checkbox = tester.firstWidget(find.byType(Checkbox)) as Checkbox;
+        final checkbox = tester.firstWidget<Checkbox>(find.byType(Checkbox));
         expect(checkbox.onChanged != null, true);
+      },
+    );
+
+    testWidgets(
+      'Call FormControl.focus() request focus on field',
+      (WidgetTester tester) async {
+        // Given: A group with a field
+        final form = FormGroup({
+          reactiveCheckboxTestingName: FormControl<bool>(),
+        });
+
+        // And: a widget that is bind to the form
+        await tester.pumpWidget(ReactiveCheckboxTestingWidget(form: form));
+
+        // Expect: that the field has no focus
+        var checkboxField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        expect(checkboxField.focusNode?.hasFocus, false);
+
+        // When: call FormControl.focus()
+        (form.control(reactiveCheckboxTestingName) as FormControl).focus();
+        await tester.pump();
+
+        // Then: the reactive field is focused
+        checkboxField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        expect(checkboxField.focusNode?.hasFocus, true);
+      },
+    );
+
+    testWidgets(
+      'Call FormControl.unfocus() remove focus on field',
+      (WidgetTester tester) async {
+        // Given: A group with a field
+        final form = FormGroup({
+          reactiveCheckboxTestingName: FormControl<bool>(),
+        });
+
+        // And: a widget that is bind to the form
+        await tester.pumpWidget(ReactiveCheckboxTestingWidget(form: form));
+
+        // And: the field has focused
+        var checkboxField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        checkboxField.focusNode?.requestFocus();
+        await tester.pump();
+        expect(checkboxField.focusNode?.hasFocus, true);
+
+        // When: call FormControl.unfocus()
+        (form.control(reactiveCheckboxTestingName) as FormControl).unfocus();
+        await tester.pump();
+
+        // Then: the reactive field is unfocused
+        checkboxField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        expect(checkboxField.focusNode?.hasFocus, false);
+      },
+    );
+
+    testWidgets(
+      'Remove focus on an invalid control show error messages',
+      (WidgetTester tester) async {
+        // Given: A group with an invalid control
+        final form = FormGroup({
+          reactiveCheckboxTestingName: FormControl<bool>(),
+        });
+
+        // And: a widget that is bind to the form
+        await tester.pumpWidget(ReactiveCheckboxTestingWidget(form: form));
+
+        // And: the field has focused
+        var checkboxField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        checkboxField.focusNode?.requestFocus();
+        await tester.pump();
+        expect(checkboxField.focusNode?.hasFocus, true);
+
+        // When: call FormControl.unfocus()
+        (form.control(reactiveCheckboxTestingName) as FormControl).unfocus();
+        await tester.pump();
+      },
+    );
+
+    testWidgets(
+      'Remove focus, and mark a control as untouched does not show error messages',
+      (WidgetTester tester) async {
+        // Given: A group with an invalid control
+        final form = FormGroup({
+          reactiveCheckboxTestingName:
+              FormControl<bool>(validators: [Validators.required]),
+        });
+
+        // And: a widget that is bind to the form
+        await tester.pumpWidget(ReactiveCheckboxTestingWidget(form: form));
+
+        // And: the field has focused
+        var textField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        textField.focusNode?.requestFocus();
+        await tester.pump();
+        expect(textField.focusNode?.hasFocus, true);
+
+        // When: call FormControl.unfocus(touched: false)
+        form.control(reactiveCheckboxTestingName).unfocus(touched: false);
+        await tester.pump();
+      },
+    );
+
+    testWidgets(
+      'Provide a FocusNode to ReactiveCheckbox',
+      (WidgetTester tester) async {
+        // Given: A group with a field
+        final form = FormGroup({
+          reactiveCheckboxTestingName: FormControl<bool>(),
+        });
+
+        // And: a focus node
+        final focusNode = FocusNode();
+
+        // And: a widget that is bind to the form
+        await tester.pumpWidget(ReactiveCheckboxTestingWidget(
+          form: form,
+          focusNode: focusNode,
+        ));
+
+        // Expect: field has the provided focus node
+        final textField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        expect(textField.focusNode, focusNode);
+      },
+    );
+
+    testWidgets(
+      'Provide a FocusNode to ReactiveCheckbox and access it through focus controller',
+      (WidgetTester tester) async {
+        // Given: A group with a field
+        final nameControl = FormControl<bool>();
+        final form = FormGroup({
+          reactiveCheckboxTestingName: nameControl,
+        });
+
+        // And: a focus node
+        final focusNode = FocusNode();
+
+        // And: a widget that is bind to the form
+        await tester.pumpWidget(ReactiveCheckboxTestingWidget(
+          form: form,
+          focusNode: focusNode,
+        ));
+
+        // Expect: field has the provided focus node and is the same of the focus controller
+        final textField = tester.firstWidget<Checkbox>(find.byType(Checkbox));
+        expect(textField.focusNode, nameControl.focusController?.focusNode);
       },
     );
   });
