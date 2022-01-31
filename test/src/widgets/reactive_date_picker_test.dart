fa07e3bbfdcd19df41151070363c07a1bb447199
Joan Pablo <johnpjm85@gmail.com>
2022-01-31 17:46:46 +0200

    add some tests to date picker widget

--- test/src/widgets/reactive_date_picker_test.dart
+++ test/src/widgets/reactive_date_picker_test.dart
@@ -128,5 +128,171 @@ void main() {
         expect(tester.takeException(), isInstanceOf<ValueAccessorException>());
       },
     );
+
+    testWidgets(
+      'Initial date of picker is DateTime.now() if value of control and initialDate argument are null ',
+      (WidgetTester tester) async {
+        // Given: a form and with null default value
+        final form = FormGroup({
+          'birthday': FormControl<DateTime>(),
+        });
+
+        // And: a widget bound to the form
+        await tester
+            .pumpWidget(ReactiveDatePickerTestingWidget<DateTime>(form: form));
+
+        // When: open picker
+        await tester.tap(find.byType(TextButton));
+        await tester.pump();
+
+        // And: get initial date of the date picker
+        final datePicker = tester.widget(find.byType(CalendarDatePicker))
+            as CalendarDatePicker;
+        final initialDate = datePicker.initialDate;
+
+        // Then: initial date if DateTime.now()
+        expect(initialDate, DateUtils.dateOnly(DateTime.now()));
+      },
+    );
+
+    testWidgets(
+      'Initial date of picker is equals to initialDate argument',
+      (WidgetTester tester) async {
+        // Given: a form and with null default value
+        final form = FormGroup({
+          'birthday': FormControl<DateTime>(),
+        });
+
+        // And: a widget bound to the form with initial Date
+        final expectedInitialDate = DateUtils.dateOnly(
+            DateTime.now().subtract(const Duration(days: 1)));
+        await tester.pumpWidget(
+          ReactiveDatePickerTestingWidget<DateTime>(
+            form: form,
+            initialDate: expectedInitialDate,
+          ),
+        );
+
+        // When: open picker
+        await tester.tap(find.byType(TextButton));
+        await tester.pump();
+
+        // And: get initial date of the date picker
+        final datePicker = tester.widget(find.byType(CalendarDatePicker))
+            as CalendarDatePicker;
+        final actualInitialDate = datePicker.initialDate;
+
+        // Then: initial date if DateTime.now()
+        expect(actualInitialDate, expectedInitialDate);
+      },
+    );
+
+    testWidgets(
+      'The initialDate argument has more priority than control value',
+      (WidgetTester tester) async {
+        // Given: a form and with null default value
+        final controlValue = DateUtils.dateOnly(
+            DateTime.now().subtract(const Duration(days: 1)));
+        final form = FormGroup({
+          'birthday': FormControl<DateTime>(value: controlValue),
+        });
+
+        // And: a widget bound to the form with initial Date
+        final expectedInitialDate = DateUtils.dateOnly(
+            DateTime.now().subtract(const Duration(days: 2)));
+        await tester.pumpWidget(
+          ReactiveDatePickerTestingWidget<DateTime>(
+            form: form,
+            initialDate: expectedInitialDate,
+          ),
+        );
+
+        // When: open picker
+        await tester.tap(find.byType(TextButton));
+        await tester.pump();
+
+        // And: get initial date of the date picker
+        final datePicker = tester.widget(find.byType(CalendarDatePicker))
+            as CalendarDatePicker;
+        final actualInitialDate = datePicker.initialDate;
+
+        // Then: initial date to widget's argument
+        expect(actualInitialDate, expectedInitialDate);
+      },
+    );
+
+    testWidgets(
+      'The initialDate is equal to firstDate if control value before firstDate',
+      (WidgetTester tester) async {
+        // Given: first date definition
+        final firstDate = DateUtils.dateOnly(
+            DateTime.now().subtract(const Duration(days: 365)));
+
+        // And: a control with a value before the firstDate value
+        final controlValue =
+            DateUtils.dateOnly(firstDate.subtract(const Duration(days: 365)));
+
+        final form = FormGroup({
+          'birthday': FormControl<DateTime>(value: controlValue),
+        });
+
+        // And: a widget bound to the form
+        await tester.pumpWidget(
+          ReactiveDatePickerTestingWidget<DateTime>(
+            form: form,
+            firstDate: firstDate,
+          ),
+        );
+
+        // When: open picker
+        await tester.tap(find.byType(TextButton));
+        await tester.pump();
+
+        // And: get initial date of the date picker
+        final datePicker = tester.widget(find.byType(CalendarDatePicker))
+            as CalendarDatePicker;
+        final actualInitialDate = datePicker.initialDate;
+
+        // Then: initial date is equals to firstDate
+        expect(actualInitialDate, firstDate);
+      },
+    );
+
+    testWidgets(
+      'The initialDate is equal to lastDate if control value after lastDate',
+          (WidgetTester tester) async {
+        // Given: first date definition
+        final lastDate = DateUtils.dateOnly(
+            DateTime.now().add(const Duration(days: 365)));
+
+        // And: a control with a value before the firstDate value
+        final controlValue =
+        DateUtils.dateOnly(lastDate.add(const Duration(days: 365)));
+
+        final form = FormGroup({
+          'birthday': FormControl<DateTime>(value: controlValue),
+        });
+
+        // And: a widget bound to the form
+        await tester.pumpWidget(
+          ReactiveDatePickerTestingWidget<DateTime>(
+            form: form,
+            lastDate: lastDate,
+          ),
+        );
+
+        // When: open picker
+        await tester.tap(find.byType(TextButton));
+        await tester.pump();
+
+        // And: get initial date of the date picker
+        final datePicker = tester.widget(find.byType(CalendarDatePicker))
+        as CalendarDatePicker;
+        final actualInitialDate = datePicker.initialDate;
+
+        // Then: initial date is equals to lastDate
+        expect(actualInitialDate, lastDate);
+      },
+    );
   });
 }
